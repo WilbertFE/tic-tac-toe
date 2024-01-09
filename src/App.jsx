81b784de2dcd3f3a62ba9482b2e4c393c0844e37
WilbertFE <wilbertbernadi699@gmail.com>
2024-01-09 18:04:59 +0700

    Project tic-tac-toe selesai

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,16 @@ function Square({ value, onSquareClick }){
   return <button className='square' onClick={onSquareClick}>{value}</button>; 
 }
 
-function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
+function Board({ xIsNext, squares, onPlay }) {
 
-  function handleClick(i){
+  function handleClick(i){   
     if(squares[i] || calculateWinner(squares)) return;
     
     const nextSquares = squares.slice();
 
     nextSquares[i] = xIsNext ? 'X' : 'O'; 
 
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
   const winner = calculateWinner(squares);
   let status = '';
@@ -47,13 +44,44 @@ function Board() {
 }
 
 export default function Game(){
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory([...history, nextSquares]); 
+    setCurrentMove(nextHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description = '';
+    if (move>0){
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
   return (
     <div className='game'>
-      <div className="game-board">
-        <Board />
+      <div className="game-board"> 
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
       </div>
       <div className="game-info">
-        <ol>{/* TO DO */}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
